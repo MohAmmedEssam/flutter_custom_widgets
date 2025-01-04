3c185b5f1d2c44463474818e05793660ee94e738
Mohammed Essam <eng.mohammedessam@gmail.com>
2025-01-04 21:59:31 +0200

    fix

--- lib/widgets/remote_image.dart
+++ lib/widgets/remote_image.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class ImageFromUrl extends StatelessWidget {
   final String? imageUrl;
@@ -8,36 +7,26 @@ class ImageFromUrl extends StatelessWidget {
   final double? radius;
   final BorderRadius? borderRadius;
   final BoxFit? fit;
+  final Widget? placeholder;
 
-  const ImageFromUrl(
-      {super.key,
-      this.imageUrl,
-      this.width = double.infinity,
-      this.height = double.infinity,
-      this.radius,
-      this.borderRadius,
-      this.fit});
+  const ImageFromUrl({
+    super.key,
+    this.imageUrl,
+    this.width = double.infinity,
+    this.height = double.infinity,
+    this.radius,
+    this.borderRadius,
+    this.fit,
+    this.placeholder,
+  });
 
   @override
   Widget build(BuildContext context) {
-    Widget defaultPlaceholder = ColorFiltered(
-      colorFilter: const ColorFilter.mode(Color(0xFFD8D8D8), BlendMode.srcIn),
-      child: ClipRRect(
-        borderRadius: borderRadius ?? BorderRadius.circular(radius ?? 0),
-        child: Image.asset(
-          "assets/images/logo.png".tr,
-          height: height,
-          width: width,
-          fit: fit,
-        ),
-      ),
-    );
-
     if ((imageUrl == null) || ((imageUrl ?? '').isEmpty)) {
       return SizedBox(
         width: width,
         height: height,
-        child: defaultPlaceholder,
+        child: placeholder ?? defaultPlaceholder(),
       );
     }
 
@@ -56,19 +45,32 @@ class ImageFromUrl extends StatelessWidget {
             return SizedBox(
               width: width,
               height: height,
-              child:
-                  defaultPlaceholder, // Display placeholder while the image is loading
+              child: placeholder ?? defaultPlaceholder(),
             );
           }
         },
         errorBuilder:
             (BuildContext context, Object error, StackTrace? stackTrace) {
           return SizedBox(
-            width: width,
-            height: height,
-            child: defaultPlaceholder, // Display placeholder while error
-          );
+              width: width,
+              height: height,
+              child: placeholder ?? defaultPlaceholder());
         },
+      ),
+    );
+  }
+
+  Widget defaultPlaceholder() {
+    return ColorFiltered(
+      colorFilter: const ColorFilter.mode(Color(0xFFD8D8D8), BlendMode.srcIn),
+      child: ClipRRect(
+        borderRadius: borderRadius ?? BorderRadius.circular(radius ?? 0),
+        child: Image.asset(
+          "assets/images/logo.png",
+          height: height,
+          width: width,
+          fit: fit,
+        ),
       ),
     );
   }
