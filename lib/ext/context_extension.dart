d3bef73b75933c8f5f049a864fe77c6d35e8bf0e
Mohammed Essam <eng.mohammedessam@gmail.com>
2025-05-10 15:03:11 +0300

    fix

--- lib/ext/context_extension.dart
+++ lib/ext/context_extension.dart
@@ -9,7 +9,7 @@ extension ContextExtension on BuildContext {
   double get fullWidth => MediaQuery.of(this).size.width;
   double get fullHeight => MediaQuery.of(this).size.height;
   double get maxWidthNeeded => min(fullWidth, 500);
-  bool get isMobileLayout => maxWidthNeeded < fullWidth;
+  bool get isMobileLayout => fullWidth <= maxWidthNeeded;
 
   // spaces
   double get hSpace => fullWidth * 0.03;
