2110002e2503ddf0872d2baa0201c6cc84d08dfd
Mohamed EssamEldin <mohamed.essameldin@onebank.eg>
2025-12-06 15:38:08 +0200

    StepIndicator

--- lib/widgets/step_indicator.dart
+++ lib/widgets/step_indicator.dart
@@ -6,46 +6,68 @@ import 'c_shadow.dart';
 import '../ext/context_extension.dart';
 
 class StepIndicator extends StatelessWidget {
-  final List<int> steps;
+  final List<String> steps;
   final int completedStep;
 
-  const StepIndicator(
-      {super.key, required this.steps, required this.completedStep});
+  const StepIndicator({
+    super.key,
+    required this.steps,
+    required this.completedStep,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          const Divider(
-            color: Colors.grey,
-            thickness: 1,
+    final stepWidth = context.width / (steps.length * 3);
+    return Stack(
+      clipBehavior: Clip.none,
+      alignment: Alignment.center,
+      children: [
+        Positioned(
+          top: (stepWidth / 2),
+          left: (stepWidth / 2),
+          right: (stepWidth / 2),
+          child: Expanded(
+            child: Container(height: 2, color: Colors.grey.shade400),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              for (var step in steps)
-                Container(
-                  color: context.colorScheme.surface,
-                  width: (context.width / (steps.length * 3)),
-                  height: (context.width / (steps.length * 3)),
-                  child: ShadowContainer(
-                    color: context.primaryColor
-                        .withAlpha(step <= completedStep ? 250 : 20),
-                    radius: context.fullHeight / 2,
-                    child: CustomText(
-                      text: '$step',
-                      fontSize: HeaderStyle.h4,
-                      alignment: Alignment.center,
-                      color: Colors.white,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: steps
+              .asMap()
+              .entries
+              .map(
+                (e) => Column(
+                  children: [
+                    Container(
+                      color: context.colorScheme.surface,
+                      width: stepWidth,
+                      height: stepWidth,
+                      child: ShadowContainer(
+                        color: context.primaryColor.withAlpha(
+                          (e.key + 1) <= completedStep ? 250 : 120,
+                        ),
+                        radius: context.fullHeight / 2,
+                        child: CustomText(
+                          text: '${(e.key + 1)}',
+                          fontSize: HeaderStyle.h4,
+                          fontWeight: FontWeight.bold,
+                          alignment: Alignment.center,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
-                  ),
+                    SizedBox(height: context.vSpace),
+                    CustomText(
+                      text: e.value,
+                      fontSize: HeaderStyle.h5,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ],
                 ),
-            ],
-          )
-        ],
-      ),
+              )
+              .toList(),
+        ),
+      ],
     );
   }
 }
